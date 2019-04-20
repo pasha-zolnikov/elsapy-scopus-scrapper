19721210b0bc07acb6ff5394ac08c2f699a76686
Pavel Zolnikov <pzolnikov@megar.ru>
2019-04-20 19:51:44 +0300

    Rewrote scrapping to using links to next batch, added scrapping for all subjects and 20th century.

--- parsing/scrape_article_xmldata.py
+++ parsing/scrape_article_xmldata.py
@@ -1,27 +1,53 @@
+import json
 import time
 import urllib
 from tqdm import tqdm
 
 
-def get_page(start, subject, date, apiKey):
-    url = 'https://api.elsevier.com/content/search/scopus?start={start}&count=25' \
-          '&query=all%28gene%29&date={date}&sort=citedby-count&subj={subject}&apiKey={key}'.format(start=start,
-                                                                                                   key=apiKey,
-                                                                                                   date=date,
-                                                                                                   subject=subject)
+def get_page(url):
     req = urllib.request.Request(url, headers={'User-Agent': "Chrome/35.0.1916.47"})
     html = urllib.request.urlopen(req).read()
     return html
 
 
-def scrape(subject, startpage, finishpage, dump_name, args):
-    for year in range(2018, 2019):
-        print(year)
-        with open(dump_name, 'wb') as dumps:
-            for start in tqdm(range(startpage, finishpage, 25)):
-                try:
-                    dump = get_page(start, subject, year, args.apikey)
-                    dumps.write(dump + b'\r\n')
-                    time.sleep(1)
-                except:
-                    break
+def make_url(subject, year, apikey, batchsize):
+    url = 'https://api.elsevier.com/content/search/scopus?cursor=*&count={batchsize}' \
+          '&query=date={date}&subj={subject}&apiKey={key}'.format(key=apikey,
+                                                                  date=year,
+                                                                  subject=subject,
+                                                                  batchsize=batchsize)
+    return url
+
+def get_next_url(dump):
+    dump_json = json.loads(dump)
+    links = dump_json['search-results']['link']
+    for link in links:
+        if link['@ref'] == 'next':
+            return link['@href']
+
+def scrape(dump_name, year, subject, args):
+    #year = args.year
+    #subject = args.subject
+    api_key = args.apikey
+    batch_size = 25
+
+    url = make_url(subject, year, api_key, batch_size)
+    print(year)
+    print(subject)
+    # print(url)
+    dump = get_page(url)
+    tree = json.loads(dump)
+    total_results = int(tree['search-results']['opensearch:totalResults'])
+    print(total_results)
+
+    with open(dump_name, 'wb') as dumps:
+        dumps.write(dump + b'\r\n')
+        for start in tqdm(range(batch_size, total_results, batch_size)):
+            url_next = get_next_url(dump)
+            try:
+                dump = get_page(url_next)
+                dumps.write(dump + b'\r\n')
+                time.sleep(2)
+            except Exception as ex:
+                print(ex)
+                break
--- parsing/scrape_main.py
+++ parsing/scrape_main.py
@@ -8,11 +8,12 @@ from argparse import ArgumentParser
 if __name__ == '__main__':
     parser = ArgumentParser(description='Scopus pages scraper')
     parser.add_argument('apikey', help='your api key')
-    parser.add_argument('startpage', help='which request page starts search', type=int)
-    parser.add_argument('finishpage', help='which request page finishes search', type=int)
+    # parser.add_argument('startpage', help='which request page starts search', type=int)
+    # parser.add_argument('finishpage', help='which request page finishes search', type=int)
     parser.add_argument('subject', help='paper subject')
-    parser.add_argument('dump_name', help='xml dump file name')
-    parser.add_argument('dataset_name', help='csv dataset file name')
+    parser.add_argument('year', help='publication year')
+    # parser.add_argument('dump_name', help='xml dump file name')
+    # parser.add_argument('dataset_name', help='csv dataset file name')
     parser.add_argument('--autorships', help='enable to extract autorships for papers', default=False, type=bool)
     parser.add_argument('--autorships_dump', help='autorships for papers dump file name')
     parser.add_argument('--citations', help='enable to extract citations for papers', default=False, type=bool)
@@ -22,14 +23,27 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    print('Start scraping')
-    scrape(args.subject, args.startpage, args.finishpage, args.dump_name, args)
-    print('Scraping finished. Start parsing dump')
-    parse(args.dump_name, args.dataset_name)
-    print('Finished parsing')
+    #year = args.year
+    #subject = args.subject
 
-    if args.autorships:
-        autorships(args.dataset_name, args.autorships_dump, args.apikey)
+    subjects = ['AGRI', 'ARTS', 'BIOC', 'BUSI', 'CENG', 'CHEM', 'COMP', 'DECI', 'DENT', 'EART', 'ECON', 'ENER', 'ENGI',
+                'ENVI', 'HEAL', 'IMMU', 'MATE', 'MATH', 'MEDI', 'NEUR', 'NURS', 'PHAR', 'PHYS', 'PSYC', 'SOCI', 'VETE',
+                'MULT']
+
+    for subject in subjects:
+        for year in range(2000, 2020):
+            year_str = str(year)
+            dumpName = subject + '_' + year_str + '_' + 'dump'
+            datasetName = subject + '_' + year_str + '_' + 'dataset'
+
+            print('Start scraping')
+            scrape(dumpName, year_str, subject, args)
+            print('Scraping finished. Start parsing dump')
+            parse(dumpName, datasetName)
+            print('Finished parsing')
+
+    # if args.autorships:
+    #     autorships(args.dataset_name, args.autorships_dump, args.apikey)
 
     # if args.citations:
     #     citations(args.dataset_name, args.citations_dump, args.citations_start, args.citations_finish)
